83e9b500f99eb5843fcbc236adf5d4221d1b18de
Anne Li <24998524+fibanneacci@users.noreply.github.com>
2021-02-12 00:26:06 -0600

    add regex for curly quotes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
+#include <regex>
 #include <algorithm>
 using namespace std;
 
 int main() {
     string str;
     getline(cin, str);
+    str = regex_replace(str, regex("“"), "\"");
+    str = regex_replace(str, regex("”"), "\"");
     replace(str.begin(), str.end(), '\"', '\'');
     cout << '\"' << str << '\"';
     return 0;
